489134d04ada2aea4d6493e8dd7f6d73611fb08e
Daniel Schoch <d.schoch90@gmail.com>
2018-10-04 23:43:32 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.string :image_url
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.string :size
       t.timestamps
     end
